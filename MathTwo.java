d3855f7518aa5265a4df807c2a4f33517f239330
clickhirphae <hpclickcomputerhope@gmail.com>
2019-06-12 15:02:21 -0400

    first commit

---
+++ MathTwo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MathTwo {
+
+    public static void main (String args[]) {
+     int mynumber=2;
+     double myothernumber=1.7938;
+
+System.out.println("my number is:"+mynumber);
+        System.out.println("My other number is:"+myothernumber);
+    }
+}
